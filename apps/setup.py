bc691de63e7b36d7a20319a5e6255dd9533ebe07
Peter Parente <parente@cs.unc.edu>
2011-03-02 16:16:22 -0500

    Added start of top-level install scripts for apps

---
+++ apps/setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+'''
+Install script for cowebx apps.
+
+Copyright (c) The Dojo Foundation 2011. All Rights Reserved.
+Copyright (c) IBM Corporation 2008, 2011. All Rights Reserved.
+'''
+
+# @todo: write
