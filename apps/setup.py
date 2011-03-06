286bb09d49fd0f453b489014dd7bd16f69d613ac
Peter Parente <parente@cs.unc.edu>
2011-03-06 15:21:16 -0500

    Python cowebx_apps deploy script

--- apps/setup.py
+++ apps/setup.py
@@ -7,12 +7,36 @@ Copyright (c) The Dojo Foundation 2011. All Rights Reserved.
 Copyright (c) IBM Corporation 2008, 2011. All Rights Reserved.
 '''
 import subprocess
+import sys
+import optparse
+import os
+import os.path
 
-def run(path):
-    # invoke pycoweb to create a deploy folder
-    # copy the files into place in an apps folder
-    # copy the widgets / styles into the deployed www/cowebx_apps
+def install(options, args):
+    # invoke pycoweb to create an empty deployment 
+    subprocess.check_call(['pycoweb', 'deploy', args[1], '-f' if options.force else ''])
+    # copy the webapps into place
+    cmd = 'cp -r src/main/webapp/* ' + os.path.join(args[1], 'www/')
+    subprocess.check_call(cmd, shell=True)
+    # copy the bots into place
+    cmd = 'cp -r src/main/python/bots/* ' + os.path.join(args[1], 'bots/')
+    subprocess.check_call(cmd, shell=True)
+    # copy the widgets / styles into place
+    cmd = 'cp -r ../widgets ' + os.path.join(args[1], 'www/lib/cowebx')
+    subprocess.check_call(cmd, shell=True)
+
+def develop(path):
     pass
 
 if __name__ == '__main__':
-    run()
+    parser = optparse.OptionParser('usage: %prog <install|develop> [options] <PATH>')
+    parser.add_option('-f', '--force', dest='force', action='store_true', default=False,
+                  help='overwrite an existing file or folder (default: False)')
+    (options, args) = parser.parse_args()
+
+    try:
+        func = globals()[args[0]]
+    except (KeyError, IndexError):
+        parser.print_usage()
+        sys.exit(255)
+    func(options, args)
