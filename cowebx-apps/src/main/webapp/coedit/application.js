3bc8ce705187a23422e99e08461a9fac6bd50dc6
Paul Bouchon <bouchon@logicalcognition.com>
2011-08-03 13:33:22 -0400

    More demodev debugging: body parse?

--- cowebx-apps/src/main/webapp/coedit/application.js
+++ cowebx-apps/src/main/webapp/coedit/application.js
@@ -25,24 +25,23 @@ define(
 		
 		var app = {
 			init: function(){
-			    this.clicked = false;
-			    parser.parse(dojo.body());
-			    
-				//1. Create the editor
-				var textEditor = new TextEditor({'domNode':dojo.byId('editorNode'),id:'textEditor',go:true});
-				
-                // var button = new Button({'domNode':textEditor.getNode(),'listenTo':textEditor,'id':'shareButton'});
-                // dojo.style(button.shareButton, 'float', 'right');
-                // dojo.style(button.shareButton, 'top', '7px');
-                // dojo.style(button.shareButton, 'left', '45px');
-                // dojo.style(button.emailBox, 'float', 'right');
-                // dojo.style(button.emailBox, 'top', '0px');
-                // dojo.style(button.emailBox, 'left', '55px');
-				//dojo.style(textEditor.getNode(), 'height', '90%');
-				
-				//2. Populate with some placeholder text and style
-				textEditor._textarea.value = 'Hello, world!\n\nTry opening two browsers both pointing to this URL, or send this link to a friend, and start typing...';
-				
+			    // this.clicked = false;
+			    // 
+			    //               //1. Create the editor
+			    //               var textEditor = new TextEditor({'domNode':dojo.byId('editorNode'),id:'textEditor',go:true});
+			    //               
+			    //                 // var button = new Button({'domNode':textEditor.getNode(),'listenTo':textEditor,'id':'shareButton'});
+			    //                 // dojo.style(button.shareButton, 'float', 'right');
+			    //                 // dojo.style(button.shareButton, 'top', '7px');
+			    //                 // dojo.style(button.shareButton, 'left', '45px');
+			    //                 // dojo.style(button.emailBox, 'float', 'right');
+			    //                 // dojo.style(button.emailBox, 'top', '0px');
+			    //                 // dojo.style(button.emailBox, 'left', '55px');
+			    //               //dojo.style(textEditor.getNode(), 'height', '90%');
+			    //               
+			    //               //2. Populate with some placeholder text and style
+			    //               textEditor._textarea.value = 'Hello, world!\n\nTry opening two browsers both pointing to this URL, or send this link to a friend, and start typing...';
+			    //               
 			   	//3. Get a session instance & prep
 			    var sess = coweb.initSession();
 			    sess.prepare();
