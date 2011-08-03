1c2a6c3e94ac9875c2bbe319a643f2e7f00c25fc
Paul Bouchon <bouchon@logicalcognition.com>
2011-08-03 13:22:43 -0400

    More demodev debugging: session timing

--- cowebx-apps/src/main/webapp/coedit/application.js
+++ cowebx-apps/src/main/webapp/coedit/application.js
@@ -12,6 +12,7 @@ define(
 		'coweb/main',
 		'dojox/mobile/parser',
 		'cowebx/dojo/BasicTextareaEditor/TextEditor',
+		//'cowebx/dojo/RichTextEditor/TextEditor',
 		'cowebx/dojo/ShareButton/Button'
 	],
 
@@ -28,17 +29,17 @@ define(
 			    	
 				//1. Create the editor
 				var textEditor = new TextEditor({'domNode':dojo.byId('editorNode'),id:'textEditor',go:true});
-				var button = new Button({'domNode':textEditor._textarea,'listenTo':textEditor._textarea,'id':'shareButton'});
-				dojo.style(button.shareButton, 'float', 'right');
-				dojo.style(button.shareButton, 'top', '62px');
-				dojo.style(button.shareButton, 'left', '45px');
-				dojo.style(button.emailBox, 'float', 'right');
-				dojo.style(button.emailBox, 'top', '56px');
-				dojo.style(button.emailBox, 'left', '57px');
-				dojo.style(textEditor._textarea, 'height', '90%');
+                // var button = new Button({'domNode':textEditor.getNode(),'listenTo':textEditor,'id':'shareButton'});
+                // dojo.style(button.shareButton, 'float', 'right');
+                // dojo.style(button.shareButton, 'top', '7px');
+                // dojo.style(button.shareButton, 'left', '45px');
+                // dojo.style(button.emailBox, 'float', 'right');
+                // dojo.style(button.emailBox, 'top', '0px');
+                // dojo.style(button.emailBox, 'left', '55px');
+				//dojo.style(textEditor.getNode(), 'height', '90%');
 				
 				//2. Populate with some placeholder text and style
-				textEditor._textarea.value = 'Hello, world!\n\nTry opening two browsers both pointing to this URL, or send this link to a friend, and start typing...';
+				//textEditor._textarea.value = 'Hello, world!\n\nTry opening two browsers both pointing to this URL, or send this link to a friend, and start typing...';
 				
 			   	//3. Get a session instance & prep
 			    var sess = coweb.initSession();
