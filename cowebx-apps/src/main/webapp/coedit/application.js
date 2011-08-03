086883c30bca92451f450842ad62dbcc423fa6dc
Paul Bouchon <bouchon@logicalcognition.com>
2011-08-03 13:26:35 -0400

    More demodev debugging: session timing

--- cowebx-apps/src/main/webapp/coedit/application.js
+++ cowebx-apps/src/main/webapp/coedit/application.js
@@ -28,7 +28,7 @@ define(
 			    this.clicked = false;
 			    	
 				//1. Create the editor
-				//var textEditor = new TextEditor({'domNode':dojo.byId('editorNode'),id:'textEditor',go:true});
+				var textEditor = new TextEditor({'domNode':dojo.byId('editorNode'),id:'textEditor',go:false});
                 // var button = new Button({'domNode':textEditor.getNode(),'listenTo':textEditor,'id':'shareButton'});
                 // dojo.style(button.shareButton, 'float', 'right');
                 // dojo.style(button.shareButton, 'top', '7px');
@@ -39,7 +39,7 @@ define(
 				//dojo.style(textEditor.getNode(), 'height', '90%');
 				
 				//2. Populate with some placeholder text and style
-				//textEditor._textarea.value = 'Hello, world!\n\nTry opening two browsers both pointing to this URL, or send this link to a friend, and start typing...';
+				textEditor._textarea.value = 'Hello, world!\n\nTry opening two browsers both pointing to this URL, or send this link to a friend, and start typing...';
 				
 			   	//3. Get a session instance & prep
 			    var sess = coweb.initSession();
