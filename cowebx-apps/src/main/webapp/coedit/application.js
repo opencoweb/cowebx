f7d3a5656a38535f1e5e89eb285a1ba43cd039d7
Paul Bouchon <bouchon@logicalcognition.com>
2011-08-03 22:22:12 -0400

    RTE: Updated Coedit with RTE for experimentation

--- cowebx-apps/src/main/webapp/coedit/application.js
+++ cowebx-apps/src/main/webapp/coedit/application.js
@@ -11,8 +11,8 @@ define(
 	    'dojo',
 		'coweb/main',
 		'dojox/mobile/parser',
-		'cowebx/dojo/BasicTextareaEditor/TextEditor',
-		//'cowebx/dojo/RichTextEditor/TextEditor',
+		//'cowebx/dojo/BasicTextareaEditor/TextEditor',
+		'cowebx/dojo/RichTextEditor/TextEditor',
 		'cowebx/dojo/ShareButton/Button'
 	],
 
@@ -29,18 +29,17 @@ define(
 			    	
 				//1. Create the editor
 				var textEditor = new TextEditor({'domNode':dojo.byId('editorNode'),id:'textEditor',go:true});
-				
-                // var button = new Button({'domNode':textEditor.getNode(),'listenTo':textEditor,'id':'shareButton'});
-                // dojo.style(button.shareButton, 'float', 'right');
-                // dojo.style(button.shareButton, 'top', '7px');
-                // dojo.style(button.shareButton, 'left', '45px');
-                // dojo.style(button.emailBox, 'float', 'right');
-                // dojo.style(button.emailBox, 'top', '0px');
-                // dojo.style(button.emailBox, 'left', '55px');
+                var button = new Button({'domNode':textEditor.getNode(),'listenTo':textEditor,'id':'shareButton'});
+                                dojo.style(button.shareButton, 'float', 'right');
+                                dojo.style(button.shareButton, 'top', '7px');
+                                dojo.style(button.shareButton, 'left', '45px');
+                                dojo.style(button.emailBox, 'float', 'right');
+                                dojo.style(button.emailBox, 'top', '0px');
+                                dojo.style(button.emailBox, 'left', '55px');
 				//dojo.style(textEditor.getNode(), 'height', '90%');
 				
 				//2. Populate with some placeholder text and style
-				textEditor._textarea.value = 'Hello, world!\n\nTry opening two browsers both pointing to this URL, or send this link to a friend, and start typing...';
+				//textEditor._textarea.value = 'Hello, world!\n\nTry opening two browsers both pointing to this URL, or send this link to a friend, and start typing...';
 				
 			   	//3. Get a session instance & prep
 			    var sess = coweb.initSession();
