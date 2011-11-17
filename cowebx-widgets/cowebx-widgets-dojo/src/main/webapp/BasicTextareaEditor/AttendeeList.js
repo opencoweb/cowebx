67cb21c53fb74ca69efdfd492827308f573941ec
Paul Bouchon <bouchon@logicalcognition.com>
2011-11-17 02:46:04 -0500

    A lot of progress on styling and syncing of niceEdit research

---
+++ cowebx-widgets/cowebx-widgets-dojo/src/main/webapp/BasicTextareaEditor/AttendeeList.js
@@ -0,0 +1,92 @@
+define([
+    'dojo',
+    'dijit/registry',
+    'coweb/main'
+], function(dojo, dijit, coweb) {
+    var AttendeeList = function(args){
+        if(!args.domNode || !args.id)
+            throw new Error("AttendeeList: missing arg");
+        this.domNode = args.domNode;
+        this.id = args.id;
+        
+        this.attendees = {};
+        this.collab = coweb.initCollab({id : this.id});
+        this.collab.subscribeSync('attendeeListJoin', this, 'onRemoteUserJoin');
+        this.collab.subscribeSync('attendeeListName', this, 'onRemoteUserName');
+    };
+    var proto = AttendeeList.prototype;
+    
+    proto.onLocalUserJoin = function(users){
+        for(var i=0; i<users.length; i++){
+            if(this.attendees[users[i]['site']]==null && users[i]['local']==true){
+                this.attendees[users[i]['site']] = {
+                    color : '#'+Math.floor(Math.random()*16777215).toString(16),
+                    name : users[i]['username']
+                };
+                this.createUserEntry(users[i]['username'],users[i]['site'],true);
+                this.collab.sendSync('attendeeListJoin', {
+                    'name':users[i]['username'],
+                    'site':users[i]['site'],
+                    'color':this.attendees[users[i]['site']]['color']
+                }, null);
+            }
+        }
+    };
+    
+    proto.onRemoteUserJoin = function(obj){
+        this.attendees[obj.value.site] = {
+            color : obj.value.color,
+            name : obj.value.name
+        };
+        this.createUserEntry(obj.value.name,obj.value.site);
+    };
+    
+    proto.onRemoteUserName = function(obj){
+        var site = obj.value.site;
+        this.attendees[site].name = obj.value.value;
+        dojo.attr('user'+site,'innerHTML',obj.value.name);
+        var color = dojo.create('div',{'class':'attendeeColor'},dojo.byId('user'+site),'first');
+        dojo.style(color,'background',this.attendees[site]['color']);
+    };
+    
+    proto.onUserLeave = function(users){
+        for(var i=0; i<users.length; i++)
+            this.destroyUserEntry(users[i]['site']);
+    };
+    
+    proto.createUserEntry = function(name, site, local){
+        var a = dojo.create('div',{id:'user'+site,'class':'attendee'},this.domNode);
+        if(local){
+            var b = dojo.create('input',{'class':'attendeeText',value:name,type:'text'},a);
+            var color = dojo.create('div',{'class':'attendeeColor'},a,'first');
+            dojo.style(color,'background',this.attendees[site]['color']);
+            dojo.addClass(a, 'localUser');
+            dojo.create('img',{src:'../lib/cowebx/dojo/RichTextEditor/images/pencil.png','class':'userEditIcon'},b,'after');
+            dojo.connect(b, 'onclick', this, function(e){ e.target.value = ''; });
+            dojo.connect(b, 'onblur', this, function(e){
+                if(e.target.value.length == 0){
+                    e.target.value = this.attendees[site].name;
+                }else{
+                    this.attendees[site].name = e.target.value;
+                    this.collab.sendSync('attendeeListName', {'name':e.target.value,'site':site}, null);
+                }
+            });
+            dojo.connect(b, 'onkeypress', this, function(e){
+                if(e.keyCode == 8)
+                    e.target.value = '';
+            });
+        }else{
+            dojo.attr(a, 'innerHTML', name);
+            var color = dojo.create('div',{'class':'attendeeColor'},a,'first');
+            dojo.style(color,'background',this.attendees[site]['color']);
+        }
+    };
+    
+    proto.destroyUserEntry = function(site){
+        dojo.destroy('user'+site);
+        dojo.destroy('caret'+site);
+        delete this.attendees[site];
+    };
+    
+    return AttendeeList;
+});
--- cowebx-widgets/cowebx-widgets-dojo/src/main/webapp/BasicTextareaEditor/TextEditor.js
+++ cowebx-widgets/cowebx-widgets-dojo/src/main/webapp/BasicTextareaEditor/TextEditor.js
@@ -1,243 +1,300 @@
-define(['coweb/main','./ld'], function(coweb,ld) {
-    var TextEditor = function(args){
-        this.id = args.id;
-        this.listen = args.listen;
-        if(!this.go)
-            this.go = true;
-        if(!this.id)
-            throw new Error('missing id argument');
-    
-        this._por = {start : 0, end: 0};
-        this._textarea = dojo.create('textarea', {}, args.domNode);
-        dojo.style(this._textarea, 'width', '600px');
-        dojo.style(this._textarea, 'height', '400px');
-        nicEditors.allTextAreas();
-		this._textarea = dojo.query('.nicEdit-main')[0];
-		this._toolbar = dojo.query('.nicEdit-panel')[0];
+define([
+    'dojo',
+	"dojo/_base/declare", // declare
+	"dijit/_Widget",
+	"dijit/_TemplatedMixin",
+	"dijit/_Contained",
+	"dojo/text!./TextEditor.html",
+	'coweb/main',
+    './ld',
+    './AttendeeList',
+    'coweb/ext/attendance',
+    'dijit/layout/ContentPane',
+    'dijit/layout/BorderContainer',
+    'dojox/mobile/parser'
+], function(dojo, declare, _Widget, _TemplatedMixin, _Contained, template, coweb, ld, AttendeeList, attendance,parser){
+
+	return declare("TextEditor", [_Widget, _TemplatedMixin, _Contained], {
+	    // widget template
+		templateString: template,
 		
-        this.oldSnapshot = this.snapshot();
-        this.newSnapshot = null;
-        this.t = null;
-        this.q = [];
-        this.value = '';
-    
-        this.collab = coweb.initCollab({id : this.id});  
-        this.collab.subscribeReady(this,'onCollabReady');
-        this.collab.subscribeSync('editorUpdate', this, 'onRemoteChange');
-        this.collab.subscribeStateRequest(this, 'onStateRequest');
-    	this.collab.subscribeStateResponse(this, 'onStateResponse');
-    
-        dojo.connect(this._textarea, 'onmousedown', this, '_updatePOR');
-        dojo.connect(this._textarea, 'onmouseup', this, '_updatePOR');
-        dojo.connect(this._textarea, 'onmousemove', this, '_updatePOR');
-        dojo.connect(this._textarea, 'onkeydown', this, '_updatePOR');
-        dojo.connect(this._textarea, 'onkeyup', this, '_updatePOR');
-        dojo.connect(this._textarea, 'onfocus', this, '_onFocus');
-        dojo.connect(this._textarea, 'onblur', this, '_onBlur');
-    
-        this.util = new ld({});
+        postCreate: function(){
+			//1. Process args
+	        this.id = 'TextEditor';
+	        this.go = true;
 
-        if(this.go == true)
-            this.listenInit();
-    };
-    var proto = TextEditor.prototype;
-    
-    proto.onCollabReady = function(){
-        this.collab.pauseSync();
-    };
+	        //2. Build stuff
+	        dojo.create('textarea', {style:'width:100%;height:100%;'}, dojo.byId('divContainer'));
+	        this._attendeeList = new AttendeeList({domNode:dojo.byId('listContainer'), id:'_attendeeList'});
+            this.util = new ld({});
+	        nicEditors.allTextAreas();
+            this._textarea = dojo.query('.nicEdit-main')[0];
+            this._toolbar = dojo.query('.nicEdit-panel')[0];
+            this.style();
+            
+            //3. parameters
+            this.oldSnapshot = this.snapshot();
+            this.newSnapshot = null;
+            this.t = null;
+            this.q = [];
+            this.value = '';
+            this.interval = 100;
+            this._por = {start : 0, end: 0};
+           
+            //4. connect
+            this.connect();
+   
+            if(this.go == true)
+               this.listenInit();
+		},
+		
+		onCollabReady : function(){
+	        this.collab.pauseSync();
+	    },
 
-    proto.listenInit = function(){
-        this.collab.pauseSync();
-        this.t = setInterval(dojo.hitch(this, 'iterate'), 2000);
-    };
-    
-    proto.iterate = function() { 
-        this.iterateSend();
-        this.iterateRecv();
-    };
-    
-    proto.iterateSend = function() {
-        this.newSnapshot = this.snapshot();
-        if(this.oldSnapshot && this.newSnapshot){
-            if(this.oldSnapshot != this.newSnapshot)
-                var syncs = this.util.ld(this.oldSnapshot, this.newSnapshot);
-            //Send syncs
-            if(syncs){
-                ///console.log('syncs pre-fix = ',syncs);
-                this.fix(syncs);
-                //console.log('syncs sent = ',syncs);
-                for(var i=0; i<syncs.length; i++){
-                    if(syncs[i] != undefined){
-                       this.collab.sendSync('editorUpdate', syncs[i].ch, syncs[i].ty, syncs[i].pos);
-                    }
-                }
-            }
-        }
-    };
-    
-    proto.fix = function(arr){
-        var temp = dojo.clone(arr);
-        var index = 0;
-        for(var i=0; i<arr.length; i++){
-            if(arr[i].ch=='<'&&arr[i+1].ch=='b'&&arr[i+2].ch=='r'&&arr[i+3].ch=='>'){
-                temp[i].ch = '<br>';
-                delete temp[i+1];
-                delete temp[i+2];
-                delete temp[i+3];
-            }else if(arr[i].ch=='&'&&arr[i+1].ch=='n'&&arr[i+2].ch=='b'&&arr[i+3].ch=='s'&&arr[i+4].ch=='p'&&arr[i+5].ch==';'){
-                temp[i].ch = '&nbsp;';
-                delete temp[i+1];
-                delete temp[i+2];
-                delete temp[i+3];
-                delete temp[i+4];
-                delete temp[i+5];
-            }    
-        }
-        return temp;
-    };
-    
-    proto.iterateRecv = function() {
-        this.collab.resumeSync();
-        this.collab.pauseSync();
-        if(this.q.length != 0)
-            this.runOps();
-        this.q = [];
-        this.oldSnapshot = this.snapshot();
-    };
-    
-    proto.onRemoteChange = function(obj){
-        this.q.push(obj);
-    };
-    
-    proto.runOps = function(){
-        this.value = this._textarea.innerHTML;
-        this._updatePOR();
-        for(var i=0; i<this.q.length; i++){
-            if(this.q[i].type == 'insert')
-                this.insertChar(this.q[i].value, this.q[i].position);
-            if(this.q[i].type == 'delete')
-                this.deleteChar(this.q[i].position);
-            if(this.q[i].type == 'update')
-                this.updateChar(this.q[i].value, this.q[i].position);
-        }
-        this._textarea.innerHTML = this.value;
-        this._moveCaretToPOR();
-    };
-        
-    proto.insertChar = function(c, pos) {
-        //this._updatePOR();
-        var t = this._textarea,
-        por = this._por,
-        start = por.start,
-        end = por.end;
-        //t.value = t.value.substr(0, pos) + c + t.value.substr(pos);
-        this.value = this.value.substr(0, pos) + c + this.value.substr(pos);
-        if(pos < por.end) {
-            if(pos >= por.start && por.end != por.start) {
-                ++start;
-            }
-            ++end;
-        }
-        if(pos < por.start) {
-            ++start;
-        }
-        por.start = start;
-        por.end = end;
-        //this._moveCaretToPOR();
-    };
-    
-    proto.insertString = function(string, pos) {
-        var x = pos;
-        for(var i=0; i<string.length; i++){
-            this.insertChar(string[i], x);
-            x++;
-        }
-    };
-    
-    proto.deleteString = function(start, end) {
-        for(var i=start; i<end; i++){
-            this.deleteChar(i);
-        }
-    };
-        
-    proto.deleteChar = function(pos) {
-        //this._updatePOR();
-        var t = this._textarea;
-        //t.value = t.value.substr(0, pos) + t.value.substr(pos+1);
-        this.value = this.value.substr(0, pos) + this.value.substr(pos+1);
-        if(pos < this._por.start) {
-            --this._por.start;
-        }
-        if(pos < this._por.end) {
-            --this._por.end;
-        }
-        //this._moveCaretToPOR();
-    };
-        
-    proto.updateChar = function(c, pos) {
-        //this._updatePOR();
-        var t = this._textarea;
-        //t.value = t.value.substr(0, pos) + c + t.value.substr(pos+1);
-        this.value = this.value.substr(0, pos) + c + this.value.substr(pos+1);
-    };
+	    listenInit : function(){
+	        this.collab.pauseSync();
+	        this.t = setTimeout(dojo.hitch(this, 'iterate'), this.interval);
+	    },
 
-    proto.snapshot = function(){
-        return this._getValueAttr();
-    };
+	    iterate : function() { 
+	        this.iterateSend();
+	        this.iterateRecv();
+	    },
 
-    proto._getValueAttr = function() {
-        return this._textarea.innerHTML;
-    };
+	    iterateSend : function() {
+	        this.newSnapshot = this.snapshot();
+	        if(this.oldSnapshot && this.newSnapshot){
+	            if(this.oldSnapshot != this.newSnapshot)
+	                var syncs = this.util.ld(this.oldSnapshot, this.newSnapshot);
+	            //Send syncs
+	            if(syncs){
+	                this.fix(syncs);
 
-    proto._moveCaretToPOR = function() {
-        if(this._focused) {
-            //this._textarea.setSelectionRange(this._por.start, this._por.end);
-        }
-    };
-        
-    proto._updatePOR = function(e) {
-        if(this._focused) {
-            var t = e ? e.target : this._textarea;
-            this._por.start = t.selectionStart;
-            this._por.end = t.selectionEnd;
-        }
-    };
-    
-    proto.setPOR = function(pos){
-        this._por.start = pos;
-        this._por.end = pos;
-    };
-        
-    proto._onFocus = function(event) {
-        this._focused = true;
-        var self = this;
-        setTimeout(function() {
-            self._moveCaretToPOR();
-        },0);
-    };
-        
-    proto._onBlur = function(event) {
-        this._focused = false;
-    };
-    
-    proto.cleanup = function() {
-        if(this.t != null){
-            clearTimeout(this.t);
-            this. t = null;
-        }
-    };
-    
-    proto.onStateRequest = function(token){
-        var state = {snapshot: this.newSnapshot};
-        this.collab.sendStateResponse(state,token);
-    };
-    
-    proto.onStateResponse = function(obj){
-        //this._textarea.value = obj.snapshot;
-        this.newSnapshot = obj.snapshot;
-        this.oldSnapshot = obj.snapshot;
-    };
-    
+	                console.log('syncs sent = ',syncs);
+	                for(var i=0; i<syncs.length; i++){
+	                    if(syncs[i] != undefined){
+	                       this.collab.sendSync('editorUpdate', syncs[i].ch, syncs[i].ty, syncs[i].pos);
+	                    }
+	                }
+	            }
+	        }
+	    },
 
-    return TextEditor;
+	    fix : function(arr){
+
+	    },
+
+	    iterateRecv : function() {
+	        this.collab.resumeSync();
+	        this.collab.pauseSync();
+	        if(this.q.length != 0)
+	            this.runOps();
+	        this.q = [];
+	        this.oldSnapshot = this.snapshot();
+	        this.t = setTimeout(dojo.hitch(this, 'iterate'), this.interval);
+	    },
+
+	    onRemoteChange : function(obj){
+	        this.q.push(obj);
+	    },
+
+	    onUserChange : function(params) {
+	        //Break if empty object
+	        if(!params.users[0])
+	            return;
+	        if(params.type == "join"){
+	            //Locally create a new listItem for the user
+	            this._attendeeList.onLocalUserJoin(params.users);
+	        }else if(params.type == "leave"){
+	            //Locally delete listItem for the user
+	            this._attendeeList.onUserLeave(params.users);
+	        }
+	    },
+
+	    runOps : function(){
+	        console.log(this.q);
+	        this.value = this._textarea.innerHTML;
+	        console.log('before = ',this.value);
+	        this._updatePOR();
+	        for(var i=0; i<this.q.length; i++){
+	            if(this.q[i].type == 'insert')
+	                this.insertChar(this.q[i].value, this.q[i].position);
+	            if(this.q[i].type == 'delete')
+	                this.deleteChar(this.q[i].position);
+	            if(this.q[i].type == 'update')
+	                this.updateChar(this.q[i].value, this.q[i].position);
+	        }
+	        console.log('after = ',this.value);
+	        this._textarea.innerHTML = this.value;
+	        this._moveCaretToPOR();
+	    },
+
+	    insertChar : function(c, pos) {
+	        //this._updatePOR();
+	        var t = this._textarea,
+	        por = this._por,
+	        start = por.start,
+	        end = por.end;
+	        //t.value = t.value.substr(0, pos) + c + t.value.substr(pos);
+	        this.value = this.value.substr(0, pos) + c + this.value.substr(pos);
+	        if(pos < por.end) {
+	            if(pos >= por.start && por.end != por.start) {
+	                ++start;
+	            }
+	            ++end;
+	        }
+	        if(pos < por.start) {
+	            ++start;
+	        }
+	        por.start = start;
+	        por.end = end;
+	        //this._moveCaretToPOR();
+	    },
+
+	    insertString : function(string, pos) {
+	        var x = pos;
+	        for(var i=0; i<string.length; i++){
+	            this.insertChar(string[i], x);
+	            x++;
+	        }
+	    },
+
+	    deleteString : function(start, end) {
+	        for(var i=start; i<end; i++){
+	            this.deleteChar(i);
+	        }
+	    },
+
+	    deleteChar : function(pos) {
+	        //this._updatePOR();
+	        var t = this._textarea;
+	        //t.value = t.value.substr(0, pos) + t.value.substr(pos+1);
+	        this.value = this.value.substr(0, pos) + this.value.substr(pos+1);
+	        if(pos < this._por.start) {
+	            --this._por.start;
+	        }
+	        if(pos < this._por.end) {
+	            --this._por.end;
+	        }
+	        //this._moveCaretToPOR();
+	    },
+
+	    updateChar : function(c, pos) {
+	        //this._updatePOR();
+	        var t = this._textarea;
+	        //t.value = t.value.substr(0, pos) + c + t.value.substr(pos+1);
+	        this.value = this.value.substr(0, pos) + c + this.value.substr(pos+1);
+	    },
+
+	    snapshot : function(){
+	        return this._getValueAttr();
+	    },
+
+	    _getValueAttr : function() {
+	        return this._textarea.innerHTML;
+	    },
+
+	    _moveCaretToPOR : function() {
+	        if(this._focused) {
+	            //this._textarea.setSelectionRange(this._por.start, this._por.end);
+	        }
+	    },
+
+	    _updatePOR : function(e) {
+	        if(this._focused) {
+	            var t = e ? e.target : this._textarea;
+	            this._por.start = t.selectionStart;
+	            this._por.end = t.selectionEnd;
+	        }
+	    },
+
+	    setPOR : function(pos){
+	        this._por.start = pos;
+	        this._por.end = pos;
+	    },
+
+	    _onFocus : function(event) {
+	        this._focused = true;
+	        var self = this;
+	        setTimeout(function() {
+	            self._moveCaretToPOR();
+	        },0);
+	    },
+
+	    _onBlur : function(event) {
+	        this._focused = false;
+	    },
+
+	    cleanup : function() {
+	        if(this.t != null){
+	            clearTimeout(this.t);
+	            this. t = null;
+	        }
+	    },
+
+	    onStateRequest : function(token){
+	        var state = {
+	            snapshot: this.newSnapshot,
+	            attendees: this._attendeeList.attendees
+	        };
+	        this.collab.sendStateResponse(state,token);
+	    },
+
+	    onStateResponse : function(obj){
+	       // this._textarea.innerHTML = obj.snapshot;
+	        this.newSnapshot = obj.snapshot;
+	        this.oldSnapshot = obj.snapshot;
+	        for(var i in obj.attendees){
+	            var o = {
+	                value: {
+	                    'site':i,
+	                    'name':obj.attendees[i]['name'],
+	                    'color':obj.attendees[i]['color']
+	                }
+	            };
+	            this._attendeeList.onRemoteUserJoin(o);
+	        }
+	    },
+
+	    connect : function(){
+	        this.collab = coweb.initCollab({id : this.id});  
+	        this.collab.subscribeReady(this,'onCollabReady');
+	        this.collab.subscribeSync('editorUpdate', this, 'onRemoteChange');
+	        this.collab.subscribeStateRequest(this, 'onStateRequest');
+	    	this.collab.subscribeStateResponse(this, 'onStateResponse');
+	        dojo.connect(this._textarea, 'onmousedown', this, '_updatePOR');
+	        dojo.connect(this._textarea, 'onmouseup', this, '_updatePOR');
+	        dojo.connect(this._textarea, 'onmousemove', this, '_updatePOR');
+	        dojo.connect(this._textarea, 'onkeydown', this, '_updatePOR');
+	        dojo.connect(this._textarea, 'onkeyup', this, '_updatePOR');
+	        dojo.connect(this._textarea, 'onfocus', this, '_onFocus');
+	        dojo.connect(this._textarea, 'onblur', this, '_onBlur');
+	        attendance.subscribeChange(this, 'onUserChange');
+	    },
+
+	    _loadTemplate : function(url) {
+	        var e = document.createElement("link");
+	        e.href = url;
+	        e.type = "text/css";
+	        e.rel = "stylesheet";
+	        e.media = "screen";
+	        document.getElementsByTagName("head")[0].appendChild(e);
+	    },
+	    
+	    style: function(){
+	        this._loadTemplate('../lib/cowebx/dojo/BasicTextareaEditor/TextEditor.css');
+	        dojo.style(this._textarea.parentNode,'width','100%');
+            dojo.style(this._textarea, 'width','100%');
+            dojo.style(this._textarea.parentNode,'height','100%');
+            dojo.style(this._textarea, 'height','100%');
+            dojo.style(this._textarea, 'margin','0px');
+            dojo.style(this._toolbar.parentNode.parentNode,'width','100%');
+            dojo.style(this._toolbar.parentNode.parentNode,'height','36px');
+            dojo.style(this._toolbar.parentNode,'height','36px');
+            dojo.style(this._toolbar,'height','36px');
+            dojo.style(this._toolbar.parentNode,'width','100%');
+            dojo.style(this._toolbar, 'width','100%');
+	    }
+	});
 });
