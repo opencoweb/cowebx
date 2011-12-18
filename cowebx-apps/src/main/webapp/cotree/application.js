80da5221fae72bf34115b49bac855b3e1e79e76b
Paul Bouchon <bouchon@logicalcognition.com>
2011-12-18 00:08:05 -0500

    Using dijit.Tree with dojo.store.Memory and dijit.tree.ObjectStoreModel for cotree

--- cowebx-apps/src/main/webapp/cotree/application.js
+++ cowebx-apps/src/main/webapp/cotree/application.js
@@ -5,57 +5,102 @@
 // 
 //
 
-define(['dojo','dojo/store/Memory','dijit/Tree','./ObjectStoreModel', 'dojo/store/Observable'],
-function(dojo, Memory, Tree, Model, Observable) {
+define([
+	'dojo',
+	'dojo/store/Memory',
+	'dijit/Tree',
+	'./ObjectStoreModel',
+	'dojo/store/Observable',
+	'dijit/tree/dndSource',
+	'dojo/aspect'],
+function(dojo, Memory, Tree, Model, Observable, dndSource, aspect) {
+	var app = {
+		init: function(){
+		   	this.currID = 0;
+			this.store 	= this._buildStore();
+			this.model 	= this._buildModel();
+			this.tree 	= this._buildTree();
+			console.log(this.tree);
+			this._buildButtons();
+		},
+		
+		// local add callback
+		//obj has new node's id, parent node's id, and new node's value
+		onLocalAddNode: function(obj){
+
+		},
+
+		// local delete callback
+		//obj has new node's id, parent node's id, and new node's value
+		onLocalDeleteNode: function(obj){
+
+		},
+
+		// local dnd callback
+		onLocalMoveNode: function(obj){
+
+		},
+
+		_buildButtons: function(){
+			//Add
+			var a = crisp.create({innerHTML:'add',domNode:dojo.byId('add')});
+			dojo.connect(a, 'onclick', this, function(){
+				this.store.add({id: this.currID, name:dojo.byId('label').value, parent: this.tree.selectedItem.id});
+				this.onLocalAddNode({
+					id: this.currID,
+					parentID: this.tree.selectedItem.id,
+					value: dojo.byId('label').value
+				});
+				this.currID++;
+			});
+			
+			//Remove
+			var d = crisp.create({innerHTML:'delete',domNode:dojo.byId('delete')});
+			dojo.connect(d, 'onclick', this, function(){
+				this.store.remove(this.tree.selectedItem.id);
+				this.onLocalDeleteNode({
+					id: this.tree.selectedItem.id,
+					parentID: this.tree.selectedItem.parent,
+					value: dojo.byId('label').value
+				});
+			});
+		},
+		
+		_buildTree: function(){
+			var tree = new Tree({
+			    model: this.model,
+				dndController: dndSource
+			});
+			tree.placeAt(document.body);
+			return tree;
+		},
+		
+		_buildModel: function(){
+			var myModel = new Model({
+		        store: this.store,
+		        query: {id: 'root'}
+		    });
+			return myModel;
+		},
+		
+		_buildStore: function(){
+			var memoryStore = new Memory({
+		        data: [{ id: 'root', name:'Phonebook'}],
+		        getChildren: function(object){ return this.query({parent: object.id}); }
+		    });
+			aspect.around(memoryStore, "put", function(originalPut){
+		        return function(obj, options){
+		            if(options && options.parent)
+		                obj.parent = options.parent.id;
+		            return originalPut.call(memoryStore, obj, options);
+		        }
+		    });
+			var observableStore = new Observable(memoryStore);
+			return observableStore;
+		}
+	};
 	
-	//1. Create store
-	var myStore = new Memory({
-        data: [
-            { id: 'world', name:'The earth', type:'planet', population: '6 billion'},
-            { id: 'AF', name:'Africa', type:'continent', population:'900 million', area: '30,221,532 sq km',
-                    timezone: '-1 UTC to +4 UTC', parent: 'world'},
-                { id: 'EG', name:'Egypt', type:'country', parent: 'AF' },
-                { id: 'KE', name:'Kenya', type:'country', parent: 'AF' },
-                    { id: 'Nairobi', name:'Nairobi', type:'city', parent: 'KE' },
-                    { id: 'Mombasa', name:'Mombasa', type:'city', parent: 'KE' },
-                { id: 'SD', name:'Sudan', type:'country', parent: 'AF' },
-                    { id: 'Khartoum', name:'Khartoum', type:'city', parent: 'SD' },
-            { id: 'AS', name:'Asia', type:'continent', parent: 'world' },
-                { id: 'CN', name:'China', type:'country', parent: 'AS' },
-                { id: 'IN', name:'India', type:'country', parent: 'AS' },
-                { id: 'RU', name:'Russia', type:'country', parent: 'AS' },
-                { id: 'MN', name:'Mongolia', type:'country', parent: 'AS' },
-            { id: 'OC', name:'Oceania', type:'continent', population:'21 million', parent: 'world'},
-            { id: 'EU', name:'Europe', type:'continent', parent: 'world' },
-                { id: 'DE', name:'Germany', type:'country', parent: 'EU' },
-                { id: 'FR', name:'France', type:'country', parent: 'EU' },
-                { id: 'ES', name:'Spain', type:'country', parent: 'EU' },
-                { id: 'IT', name:'Italy', type:'country', parent: 'EU' },
-            { id: 'NA', name:'North America', type:'continent', parent: 'world' },
-            { id: 'SA', name:'South America', type:'continent', parent: 'world' }
-        ],
-        getChildren: function(object){
-            return this.query({parent: object.id});
-        }
+	dojo.ready(function() {
+        app.init();
     });
-	myStore = new Observable(myStore);
-	
-	//2. Create model
-    var myModel = new Model({
-        store: myStore,
-        query: {id: 'world'}
-    });
-	
-	//3. Create tree
-	var tree = new Tree({
-	    model: myModel
-	});
-	tree.placeAt(document.body);
-	
-	//4. Create buttons
-	var b = crisp.create({innerHTML:'add',domNode:dojo.byId('add')});
-	dojo.connect(b,'onclick',this,function(){
-		myStore.add({id: 'US', name:'United States', type:'country', parent: 'NA'});
-	});
-	
 });
