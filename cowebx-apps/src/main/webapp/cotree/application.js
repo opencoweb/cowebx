592e87789635e6ccc3c2e61b296c3245630b379e
Paul Bouchon <bouchon@logicalcognition.com>
2011-12-19 01:02:21 -0500

    Switched to an ItemFileWriteStore with TreeStoreModel

--- cowebx-apps/src/main/webapp/cotree/application.js
+++ cowebx-apps/src/main/webapp/cotree/application.js
@@ -7,55 +7,54 @@
 
 define([
 	'dojo',
-	'dojo/store/Memory',
+	'dijit/dijit',
+	'dojo/data/ItemFileWriteStore',
 	'dijit/Tree',
-	'./ObjectStoreModel',
-	'dojo/store/Observable',
+	'dijit/tree/TreeStoreModel',
 	'dijit/tree/dndSource',
-	'dojo/aspect'],
-function(dojo, Memory, Tree, Model, Observable, dndSource, aspect) {
+	'dijit/Menu',
+	'dijit/form/Button',
+	'dojo/dnd/common',
+	'dojo/dnd/Source'],
+function(dojo, dijit, Store, Tree, Model, dndSource, Menu, Button) {
 	var app = {
 		init: function(){
-			this.data = [
-	            {id:'root', name:'Phonebook'},
-					{id: 1, name:'person', parent:'root'},
-						{id: 2, name:'First Name', parent:1},
-							{id: 3, name:'Paul', parent:2},
-						{id: 4, name:'Last Name', parent:1},
-							{id: 5, name:'Bouchon', parent:4},
-						{id: 6, name:'Address', parent:1},
-							{id: 7, name:'Work', parent:6},
-								{id: 8, name:'Street', parent:7},
-									{id: 9, name:'101 Foobar Way', parent:8},
-								{id: 10, name:'City', parent:7},
-									{id: 11, name:'Chapel Hill', parent:10},
-								{id: 12, name:'State', parent:7},
-									{id: 13, name:'North Carolina', parent:12},
-					{id: 14, name:'person', parent:'root'},
-						{id: 15, name:'First Name', parent:14},
-							{id: 16, name:'Brian', parent:15},
-						{id: 17, name:'Last Name', parent:14},
-							{id: 18, name:'Burns', parent:17},
-						{id: 19, name:'Address', parent:14},
-							{id: 20, name:'Work', parent:19},
-								{id: 21, name:'Street', parent:20},
-									{id: 22, name:'107 JS Drive', parent:21},
-								{id: 23, name:'City', parent:20},
-									{id: 24, name:'Palo Alto', parent:23},
-								{id: 25, name:'State', parent:20},
-									{id: 26, name:'California', parent:25}
-			];
-			this.currID = 27;
-			this.store 	= this._buildStore();
-			this.model 	= this._buildModel();
-			this.tree 	= this._buildTree();
+			selected=[];
+			this.globalID=1000;
+			window.foo = this;
+			
+			this.data = {
+				identifier: 'id',
+				label: 'name',
+				items: [
+				
+				
+				
+				
+					{ id: '0', name:'Foods', children:[ {_reference: '1'},  {_reference: '2'},  {_reference: '3'} ] },
+						{ id: '1', name:'Fruits', children:[ {_reference: '4'} ] },
+							{ id: '4', name:'Citrus', children:[ {_reference: 'def'} ] },
+								{id: 'def',name:'foobar'},
+						{ id: '2', name:'Vegetables'},
+						{ id: '3', name:'Cereals'}
+				]
+			};
+			this.store = new Store({data:this.data});
+			this.model = new Model({store:this.store, query:{id:"0"}});
+			this.tree = new Tree({
+				id:'thisTree',
+				'class':'container',
+				model:this.model,
+				betweenThreshold:5,
+				persist:false,
+				dndController:dndSource
+			});
+			this.tree.placeAt(dojo.byId('treeContainer'));
 			this._buildButtons();
 			
-			
-			dojo.connect(window, 'onkeypress', this, function(e){
-				if(e.keyCode == 13)
-					console.log(this.tree);
-			})
+			dojo.subscribe("/dnd/drop", function(obj){
+			    console.log('drop',obj);
+			});
 		},
 		
 		// local add callback
@@ -72,7 +71,6 @@ function(dojo, Memory, Tree, Model, Observable, dndSource, aspect) {
 		// obj : {
 	 	//     id		: id of deleted node
 		//     parentID	: id of deleted node's parent
-		//	   value	: value of deleted node
 		// }
 		onLocalDeleteNode: function(obj){
 
@@ -82,38 +80,32 @@ function(dojo, Memory, Tree, Model, Observable, dndSource, aspect) {
 		// obj : {
 		// }
 		onLocalMoveNode: function(obj){
-
+			
+		},
+		
+		// refreshes tree based on updates to model
+		refreshTree: function(){
+			dijit.byId('thisTree').destroyRecursive();
+			this.model = new Model({store:this.store, query:{id:"0"}});
+			this.tree = new Tree({
+				id:'thisTree',
+				'class':'container',
+				model:this.model,
+				betweenThreshold:5,
+				persist:false,
+				dndController:dndSource
+			});
+			this.tree.placeAt(dojo.byId('treeContainer'));
 		},
 
 		_buildButtons: function(){
 			//Add
 			var a = crisp.create({innerHTML:'add',domNode:dojo.byId('add'),width:"250px"});
-			dojo.connect(a, 'onclick', this, function(){
-				if(this.tree.selectedNode != null){
-					this.store.add({id: this.currID, name:dojo.byId('label').value, parent: this.tree.selectedItem.id});
-					this.onLocalAddNode({
-						id: this.currID,
-						parentID: this.tree.selectedItem.id,
-						value: dojo.byId('label').value
-					});
-					this.currID++;
-					dojo.style('label', 'color', 'lightgrey');
-					dojo.byId('label').value = 'Node label...';
-				}else{
-					alert("To add a node, select a parent node in the tree and enter a label.");
-				}
-			});
+			dojo.connect(a, 'onclick', this, '_addNode');
 			
 			//Remove
 			var d = crisp.create({innerHTML:'delete',domNode:dojo.byId('delete'),bgColor:'red',width:'250px'});
-			dojo.connect(d, 'onclick', this, function(){
-				this.store.remove(this.tree.selectedItem.id);
-				this.onLocalDeleteNode({
-					id: this.tree.selectedItem.id,
-					parentID: this.tree.selectedItem.parent,
-					value: dojo.byId('label').value
-				});
-			});
+			dojo.connect(d, 'onclick', this, '_deleteNode');
 			
 			//Connect UI events
 			dojo.connect(dojo.byId('label'),'onfocus',this,function(e){
@@ -129,39 +121,45 @@ function(dojo, Memory, Tree, Model, Observable, dndSource, aspect) {
 				}	
 			});
 		},
-		
-		_buildTree: function(){
-			var tree = new Tree({
-			    model: this.model,
-				dndController: dndSource,
-				betweenThreshold:5
-			});
-			tree.placeAt(dojo.byId('treeContainer'));
-			return tree;
+
+		_addNode: function(){
+			if((this.tree.selectedNode != null) && (dojo.byId('label').value != ('Node label...' || '')) ){
+				var newNode = this.store.newItem({ id: this.globalID.toString(), name:dojo.byId('label').value});
+				var parentID = this.tree.selectedItem.id[0];
+				this.store.fetch({query:{id:parentID},onComplete: dojo.hitch(this,function(items){
+					var children = this.tree.selectedItem.children;
+					if(children == undefined)
+						children = [];
+					children.push(newNode);
+					this.store.setValue(items[0],'children',children);
+					this.store.save({onComplete: dojo.hitch(this, 'refreshTree')});
+				})});
+				this.onLocalAddNode({
+					id: this.globalID.toString(),
+					parentID: parentID,
+					value: dojo.byId('label').value
+				});
+				this.globalID++;
+				dojo.style('label', 'color', 'lightgrey');
+				dojo.byId('label').value = 'Node label...';
+			}else{
+				alert("To add a node, select a parent node in the tree and enter a label.");
+			}
 		},
 		
-		_buildModel: function(){
-			var myModel = new Model({
-		        store: this.store,
-		        query: {id: 'root'}
-		    });
-			return myModel;
-		},
-		
-		_buildStore: function(){
-			var memoryStore = new Memory({
-		        data: this.data,
-		        getChildren: function(object){ return this.query({parent: object.id}); }
-		    });
-			aspect.around(memoryStore, "put", function(originalPut){
-		        return function(obj, options){
-		            if(options && options.parent)
-		                obj.parent = options.parent.id;
-		            return originalPut.call(memoryStore, obj, options);
-		        }
-		    });
-			var observableStore = new Observable(memoryStore);
-			return observableStore;
+		_deleteNode: function(){
+			if(this.tree.selectedNode != null){
+				var targetItem = this.tree.selectedItem;
+				var parentItem = this.tree.selectedNode.getParent().item;
+				this.store.deleteItem(targetItem);
+				this.store.save({onComplete:dojo.hitch(this, 'refreshTree')});
+				this.onLocalDeleteNode({
+					id: targetItem.id[0],
+					parentID: parentItem.id[0]
+				});
+			}else{
+				alert("You must select a node to delete.");
+			}
 		}
 	};
 	
