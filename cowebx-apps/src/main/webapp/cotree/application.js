74f6c61a20520202c3ce30ede8c413bd9c8b2b71
Paul Bouchon <bouchon@logicalcognition.com>
2011-12-18 19:11:37 -0500

    UI updates for cotree

--- cowebx-apps/src/main/webapp/cotree/application.js
+++ cowebx-apps/src/main/webapp/cotree/application.js
@@ -16,46 +16,96 @@ define([
 function(dojo, Memory, Tree, Model, Observable, dndSource, aspect) {
 	var app = {
 		init: function(){
-		   	this.currID = 0;
+			this.data = [
+	            {id:'root', name:'Phonebook'},
+					{id: 1, name:'person', parent:'root'},
+						{id: 2, name:'First Name', parent:1},
+							{id: 3, name:'Paul', parent:2},
+						{id: 4, name:'Last Name', parent:1},
+							{id: 5, name:'Bouchon', parent:4},
+						{id: 6, name:'Address', parent:1},
+							{id: 7, name:'Work', parent:6},
+								{id: 8, name:'Street', parent:7},
+									{id: 9, name:'101 Foobar Way', parent:8},
+								{id: 10, name:'City', parent:7},
+									{id: 11, name:'Chapel Hill', parent:10},
+								{id: 12, name:'State', parent:7},
+									{id: 13, name:'North Carolina', parent:12},
+					{id: 14, name:'person', parent:'root'},
+						{id: 15, name:'First Name', parent:14},
+							{id: 16, name:'Brian', parent:15},
+						{id: 17, name:'Last Name', parent:14},
+							{id: 18, name:'Burns', parent:17},
+						{id: 19, name:'Address', parent:14},
+							{id: 20, name:'Work', parent:19},
+								{id: 21, name:'Street', parent:20},
+									{id: 22, name:'107 JS Drive', parent:21},
+								{id: 23, name:'City', parent:20},
+									{id: 24, name:'Palo Alto', parent:23},
+								{id: 25, name:'State', parent:20},
+									{id: 26, name:'California', parent:25}
+			];
+			this.currID = 27;
 			this.store 	= this._buildStore();
 			this.model 	= this._buildModel();
 			this.tree 	= this._buildTree();
-			console.log(this.tree);
 			this._buildButtons();
+			
+			
+			dojo.connect(window, 'onkeypress', this, function(e){
+				if(e.keyCode == 13)
+					console.log(this.tree);
+			})
 		},
 		
 		// local add callback
-		//obj has new node's id, parent node's id, and new node's value
+		// obj : {
+	 	//     id		: id of added node
+		//     parentID	: id of added node's parent
+		//	   value	: value of added node
+		// }
 		onLocalAddNode: function(obj){
 
 		},
 
 		// local delete callback
-		//obj has new node's id, parent node's id, and new node's value
+		// obj : {
+	 	//     id		: id of deleted node
+		//     parentID	: id of deleted node's parent
+		//	   value	: value of deleted node
+		// }
 		onLocalDeleteNode: function(obj){
 
 		},
 
 		// local dnd callback
+		// obj : {
+		// }
 		onLocalMoveNode: function(obj){
 
 		},
 
 		_buildButtons: function(){
 			//Add
-			var a = crisp.create({innerHTML:'add',domNode:dojo.byId('add')});
+			var a = crisp.create({innerHTML:'add',domNode:dojo.byId('add'),width:"250px"});
 			dojo.connect(a, 'onclick', this, function(){
-				this.store.add({id: this.currID, name:dojo.byId('label').value, parent: this.tree.selectedItem.id});
-				this.onLocalAddNode({
-					id: this.currID,
-					parentID: this.tree.selectedItem.id,
-					value: dojo.byId('label').value
-				});
-				this.currID++;
+				if(this.tree.selectedNode != null){
+					this.store.add({id: this.currID, name:dojo.byId('label').value, parent: this.tree.selectedItem.id});
+					this.onLocalAddNode({
+						id: this.currID,
+						parentID: this.tree.selectedItem.id,
+						value: dojo.byId('label').value
+					});
+					this.currID++;
+					dojo.style('label', 'color', 'lightgrey');
+					dojo.byId('label').value = 'Node label...';
+				}else{
+					alert("To add a node, select a parent node in the tree and enter a label.");
+				}
 			});
 			
 			//Remove
-			var d = crisp.create({innerHTML:'delete',domNode:dojo.byId('delete')});
+			var d = crisp.create({innerHTML:'delete',domNode:dojo.byId('delete'),bgColor:'red',width:'250px'});
 			dojo.connect(d, 'onclick', this, function(){
 				this.store.remove(this.tree.selectedItem.id);
 				this.onLocalDeleteNode({
@@ -64,14 +114,29 @@ function(dojo, Memory, Tree, Model, Observable, dndSource, aspect) {
 					value: dojo.byId('label').value
 				});
 			});
+			
+			//Connect UI events
+			dojo.connect(dojo.byId('label'),'onfocus',this,function(e){
+				dojo.style(e.target,'color','black');
+				if(e.target.value == 'Node label...')
+					e.target.value = '';
+			});
+			
+			dojo.connect(dojo.byId('label'),'onblur',this,function(e){
+				if(e.target.value == ''){
+					dojo.style(e.target,'color','lightgrey');
+					e.target.value = 'Node label...';
+				}	
+			});
 		},
 		
 		_buildTree: function(){
 			var tree = new Tree({
 			    model: this.model,
-				dndController: dndSource
+				dndController: dndSource,
+				betweenThreshold:5
 			});
-			tree.placeAt(document.body);
+			tree.placeAt(dojo.byId('treeContainer'));
 			return tree;
 		},
 		
@@ -85,7 +150,7 @@ function(dojo, Memory, Tree, Model, Observable, dndSource, aspect) {
 		
 		_buildStore: function(){
 			var memoryStore = new Memory({
-		        data: [{ id: 'root', name:'Phonebook'}],
+		        data: this.data,
 		        getChildren: function(object){ return this.query({parent: object.id}); }
 		    });
 			aspect.around(memoryStore, "put", function(originalPut){
