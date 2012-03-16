d15d624b25cf4c97229715c8c79515d1051f2813
Paul Bouchon <bouchon@logicalcognition.com>
2012-03-16 11:50:20 -0400

    Issue #171 widgetize editor

--- cowebx-apps/src/main/webapp/coedit/application.js
+++ cowebx-apps/src/main/webapp/coedit/application.js
@@ -10,44 +10,18 @@ define(
 	    'dojo',
 		'coweb/main',
 		'dojox/mobile/parser',
-		'cowebx/dojo/RichTextEditor/TextEditor',
+		'cowebx/dojo/RichTextEditor/RichTextEditor',
 	],
 
 	function(
 	    dojo,
 		coweb,
 		parser,
-		TextEditor) {
+		RichTextEditor) {
 		
 		var app = {
 			init: function(){
 			    dojo.parser.parse();
-
-			    //1. Generate session or enter session
- 			    if(location.hash == ''){
- 			        	dojo.style('splash','display','block');
- 			        	console.log("hashtag = "+location.hash);
-	       			dojo.connect(dojo.byId('newDoc'),'onclick',this,function(){
-	       				window.location = document.URL+'#/cowebkey/'+Math.floor(Math.random()*10000001);
-	       				window.location.reload();
-	       			});
-			    }else{
-	        	    		var sess = coweb.initSession();
-    			    	sess.prepare().then(function(info) { });
-   			        	dojo.style('editorNode','display','block');
-   			        	dojo.fadeIn({node:'editorNode',duration:1000}).play();
-			    }
-			},
-			
-			aquireUrlParams: function(param){
-				param = param.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
-				var pattern = "[\\?&]"+param+"=([^&#]*)";
-				var regex = new RegExp( pattern );
-				var results = regex.exec( window.location.href );
-				if( results == null )
-					return null;
-				else
-					return results[1];
 			}
 		};
 		
