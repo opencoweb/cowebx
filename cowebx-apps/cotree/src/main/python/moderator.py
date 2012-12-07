66c10873a79ab0fc3c21ee022ca3193438daea2f
Chris Cotter <ccotter@utexas.edu>
2012-12-07 14:02:03 -0600

    setup script moved to base of cowebx-apps and should work for all apps. Still need to add code to copy/symlink updater classes, though #203

---
+++ cowebx-apps/cotree/src/main/python/moderator.py
@@ -0,0 +1,93 @@
+
+import json
+from coweb.moderator import DefaultSessionModerator
+
+class CotreeModerator(DefaultSessionModerator):
+
+    def __init__(self):
+        super(DefaultSessionModerator, self).__init__()
+        self.arr = json.loads(_initialState)
+        self.tmpChild = None
+
+    def getLateJoinState(self):
+        return {"shoppinglist": self.bgData}
+
+    def onSync(self, client, data):
+        value = data.get("value")
+        ty = data.get("type")
+        force = value.get("force", False)
+        pos = data.get("position")
+        if "insert" == ty:
+            self.insert(value, pos, force)
+        elif "delete" == ty:
+            self.remove(value, pos, force)
+        elif "update" == ty:
+            self.update(value, pos)
+
+    def update(self, value, pos):
+        parent = self.findNode(value.get("parentId"))
+        if parent is None:
+            return
+        children = parent.get("children")
+        if pos >= len(children):
+            return
+        children[pos]["name"] = value.get("name")
+
+    def insert(self, value, pos, force):
+        if force:
+            node = {"id": value.get("id"),
+                    "name": value.get("value"),
+                    "children": []
+                    }
+            parent = self.findNode(value.get("parentId"))
+        else:
+            node = self.tmpChild
+            parent = self.findNode(value.get("newParentID"))
+        if parent is None:
+            return
+        children = parent.get("children")
+        if pos > len(children):
+            return
+        children.insert(pos, node)
+
+    def remove(self, value, pos, force):
+        if force:
+            parent = self.findNode(value.get("parentId"))
+        else:
+            parent = self.findNode(value.get("prevParentID"))
+        if parent is None:
+            return
+        children = parent.get("children")
+        if pos >= len(children):
+            return
+        self.tmpChild = children.pop(pos)
+
+    def findNode(self, _id):
+        return self.findNodeRecursive(self.arr, _id)
+
+    def findNodeRecursive(self, _map, _id):
+        if _map["id"] == _id:
+            return _map
+        ch = _map["children"]
+        for c in ch:
+            r = self.findNodeRecursive(c, _id)
+            if r is not None:
+                return r
+        return None
+
+    def getLateJoinState(self):
+        return {"phonebook": self.arr}
+
+    def canClientSubscribeService(self, svcName, cl, msg):
+        return True
+
+    def canClientMakeServiceRequest(self, svcName, cl, msg):
+        return True
+
+    def onSessionReady(self):
+        pass
+
+    def onSessionEnd(self):
+        pass
+
+_initialState = "{\"id\":\"root\", \"name\":\"Phonebook\",\"children\":[{\"id\":\"0\",\"name\":\"Person\",\"children\":[{\"id\":\"1\",\"name\":\"firstname\",\"children\":[{\"id\":\"2\",\"name\":\"Paul\",\"children\":[]}]},{\"id\":\"3\",\"name\":\"lastname\", \"children\":[ { \"id\":\"4\", \"name\":\"Bouchon\", \"children\":[] } ] }, { \"id\":\"5\", \"name\":\"address\", \"children\":[ { \"id\":\"6\", \"name\":\"home\", \"children\":[ { \"id\":\"7\", \"name\":\"street\", \"children\":[ { \"id\":\"8\", \"name\":\"101 Happy Drive\", \"children\":[] } ] }, { \"id\":\"9\", \"name\":\"city\", \"children\":[ { \"id\":\"10\", \"name\":\"New Orleans\", \"children\":[] } ] } ] } ] } ] }, { \"id\":\"11\", \"name\":\"Person\", \"children\":[ { \"id\":\"12\", \"name\":\"firstname\", \"children\":[ { \"id\":\"13\", \"name\":\"Dan\", \"children\":[] } ] }, { \"id\":\"14\", \"name\":\"lastname\", \"children\":[ { \"id\":\"15\", \"name\":\"Gisolfi\", \"children\":[] } ] }, { \"id\":\"16\", \"name\":\"address\", \"children\":[ { \"id\":\"17\", \"name\":\"home\", \"children\":[ { \"id\":\"18\", \"name\":\"street\", \"children\":[ { \"id\":\"19\", \"name\":\"102 1337 Way\", \"children\":[] } ] }, { \"id\":\"20\", \"name\":\"city\", \"children\":[ { \"id\":\"21\", \"name\":\"Palo Alto\", \"children\":[] } ] } ] } ] } ] }, { \"id\":\"22\", \"name\":\"Person\", \"children\":[ { \"id\":\"23\", \"name\":\"firstname\", \"children\":[ { \"id\":\"24\", \"name\":\"Brian\", \"children\":[] } ] }, { \"id\":\"25\", \"name\":\"lastname\", \"children\":[ { \"id\":\"26\", \"name\":\"Burns\", \"children\":[] } ] }, { \"id\":\"27\", \"name\":\"address\", \"children\":[ { \"id\":\"28\", \"name\":\"home\", \"children\":[ { \"id\":\"29\", \"name\":\"street\", \"children\":[ { \"id\":\"30\", \"name\":\"103 Windoze Drive\", \"children\":[] } ] }, { \"id\":\"31\", \"name\":\"city\", \"children\":[ { \"id\":\"32\", \"name\":\"New York\", \"children\":[] } ] } ] } ] } ] } ] }";
---
+++ cowebx-apps/setup.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python
+'''
+Install script for cowebx apps. Requires prior install of Python coweb
+server package and pycoweb script.
+
+If this script is run from within an activated virtual env, it will be the
+default install path. Otherwise, the install path must be explicitly given
+(and can override using the virtual env path).
+
+Copyright (c) The Dojo Foundation 2011. All Rights Reserved.
+Copyright (c) IBM Corporation 2008, 2011. All Rights Reserved.
+'''
+import subprocess
+import sys
+import argparse
+import os
+import os.path
+
+class SetupError(Exception): pass
+
+def rm(target, f):
+	subprocess.call(["rm", "-rf", os.path.join(target, f)])
+
+def ln(src, target):
+	subprocess.call(["ln", "-s", src, target])
+
+def _symlink_path(srcRoot, target):
+	for path in os.listdir(srcRoot):
+		if path.startswith('.') or path == 'WEB-INF': continue
+		src = os.path.abspath(os.path.join(srcRoot, path))
+		dest = os.path.join(target, path)
+		try:
+			os.symlink(src, dest)
+		except OSError:
+			print('skipped: %s' % path)
+
+def deploy(args):
+	'''Deploys the sample apps and their widgets into coweb deploy dir.'''
+	# invoke pycoweb to create an empty deployment 
+	try:
+		dest = args.path
+	except IndexError:
+		raise SetupError('missing: destination path')
+
+	pref = args.app + '/src/main'
+
+	if subprocess.call([
+		'pycoweb', 'deploy', dest, 
+		'-f' if args.force else '',
+		'-t', pref + '/python/run_server.tmpl']):
+		raise SetupError('aborted: cowebx deploy')
+
+	# copy the webapps into place
+	cmd = 'cp -r ' + pref + '/webapp/* ' + os.path.join(dest, 'www/')
+	subprocess.check_call(cmd, shell=True)
+
+	if os.path.exists(pref + "/python/bots"):
+		# copy the bots into place
+		cmd = 'cp -r ' + pref + '/python/bots/* ' + os.path.join(dest, 'bots/')
+		subprocess.check_call(cmd, shell=True)
+
+	if os.path.exists(pref + "/python/moderator.py"):
+		rm(dest, 'bin/moderator.py')
+		cmd = 'cp ' + pref + '/python/moderator.py ' + os.path.join(dest, 'bin')
+		subprocess.check_call(cmd, shell=True)
+
+	# copy the widgets / styles into place
+	try:
+		os.makedirs(os.path.join(dest, 'www/lib/cowebx'))
+	except OSError:
+		pass
+	cmd = 'cp -r ../cowebx-widgets/cowebx-widgets-dojo/src/main/webapp ' + \
+		  os.path.join(dest, 'www/lib/cowebx/dojo')
+	subprocess.check_call(cmd, shell=True)
+
+def develop(args):
+	'''Symlinks the apps and widgets into an existing developer env.'''
+	try:
+		dest = args.path
+	except IndexError:
+		raise SetupError('missing: destination path')
+
+	pref = args.app + '/src/main'
+
+	# create container script
+	if subprocess.call([
+		'pycoweb', 'deploy', dest, 
+		'-f' if args.force else '',
+		'-t', pref + '/python/run_server.tmpl']):
+		raise SetupError('aborted: cowebx develop')
+
+	# symlink the webapps
+	target = os.path.join(dest, 'www')
+	_symlink_path(pref + '/webapp', target)
+	
+	if os.path.exists(pref + "/python/bots"):
+		# symlink bots into bots/
+		target = os.path.join(dest, 'bots')
+		rm(dest, 'bots')
+		os.mkdir(target)
+		_symlink_path(pref + '/python/bots', target)
+
+	if os.path.exists(pref + "/python/moderator.py"):
+		# symlink moderator
+		rm(dest, 'bin/moderator.py')
+		ln(os.path.abspath(pref + '/python/moderator.py'),
+				os.path.join(dest, 'bin/moderator.py'))
+
+	# symlink widgets into www/lib/cowebx
+	try:
+		os.makedirs(os.path.join(dest, 'www/lib/cowebx'))
+	except OSError:
+		pass
+	target = os.path.join(dest, 'www/lib/cowebx/dojo')
+	src = os.path.abspath('../cowebx-widgets/cowebx-widgets-dojo/src/'
+			'main/webapp')
+	try:
+		os.symlink(src, target)
+	except OSError as e:
+		print('skipped: cowebx-widgets-dojo')
+
+def clean(args):
+	'''Cleans up the workpath files created by this script.'''
+	try:
+		dest= args.path
+	except IndexError:
+		raise SetupError('missing: destination path')
+	rm(dest, 'bin/run_server.py')
+	rm(dest, 'bin/moderator.py')
+	rm(dest, 'www')
+	rm(dest, 'bots')
+
+if __name__ == '__main__':
+	defaultPath = os.environ.get('VIRTUAL_ENV')
+	parser = argparse.ArgumentParser(prog='setup.py')
+	parser.add_argument('-f', '--force', dest='force', action='store_true',
+			default=False,
+			help='overwrite an existing file or folder (default: False)')
+	parser.add_argument('-a', '--app', action='store')
+	parser.add_argument('action', action='store', choices=['deploy', 'develop',
+		'clean'])
+	parser.add_argument('-p', '--path', action='store', default=defaultPath)
+	args = parser.parse_args()
+
+	# Doesn't matter what the app is when we clean.
+	if "clean" != args.action:
+		if args.app is None:
+			parser.print_usage();
+			sys.exit(1)
+		elif not os.path.exists(args.app):
+			print("Path " + args.app + " does not exist")
+			sys.exit(1)
+
+	try:
+		func = globals()[args.action]
+	except Exception:
+		parser.print_usage()
+		sys.exit(1)
+	try:
+		func(args)
+	except SetupError as e:
+		print(e)
+		sys.exit(1)
